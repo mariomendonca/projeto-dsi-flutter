8ba40d52747d41b2c13c3eb619f1755c53dc2445
CarlosHenrique <carlossouzap@outlook.com>
2021-06-01 11:37:14 -0300

    Alterando os nomes dos widgets e da pasta

---
+++ lib/screens/ConsultaMarcada/marcar_consulta.dart
@@ -0,0 +1,24 @@
+import 'package:cmedapp/components/AppBar.dart';
+import 'package:cmedapp/screens/ConsultaMarcada/body.dart';
+
+import 'package:flutter/material.dart';
+
+class MarcandoConsulta extends StatefulWidget {
+  @override
+  _MarcandoConsultaState createState() => _MarcandoConsultaState();
+}
+
+class _MarcandoConsultaState extends State<MarcandoConsulta> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[50],
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(size.height * 0.10),
+        child: AppBar2(),
+      ),
+      body: BodyConsultaMarcada(),
+    );
+  }
+}
