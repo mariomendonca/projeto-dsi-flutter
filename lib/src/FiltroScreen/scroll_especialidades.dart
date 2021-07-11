edcc6f39384f1e02674f625162eb1379e52a5657
samuelefs <samuel.souza.ernandes@hotmail.com>
2021-07-11 15:08:04 -0300

    tela de filtro

--- lib/src/FiltroScreen/scroll_especialidades.dart
+++ lib/src/FiltroScreen/scroll_especialidades.dart
@@ -1,3 +1,4 @@
+import 'package:cmedapp/src/AllDoctors/all_doctors.dart';
 import 'package:cmedapp/src/FiltroScreen/box_especialidade.dart';
 import 'package:cmedapp/src/FiltroScreen/filter_screen.dart';
 import 'package:flutter/material.dart';
@@ -13,25 +14,74 @@ class ScrollEspecialidades extends StatelessWidget {
       scrollDirection: Axis.vertical,
       child: Column(
         children: [
-          BoxEspecialidade(especialidade: "Cardiologista", press: () {}),
+          BoxEspecialidade(
+              especialidade: "Cardiologista",
+              press: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => AllDoctors(
+                          especialidade: "cardiologista",
+                        )));
+              }),
           Espaco(),
-          BoxEspecialidade(especialidade: "Otorrino", press: () {}),
+          BoxEspecialidade(
+              especialidade: "Nutricionista",
+              press: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => AllDoctors(
+                          especialidade: "nutricionista",
+                        )));
+              }),
           Espaco(),
-          BoxEspecialidade(especialidade: "Cardiologista", press: () {}),
+          BoxEspecialidade(
+              especialidade: "Oftalmologista",
+              press: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => AllDoctors(
+                          especialidade: 'oftalmologista',
+                        )));
+              }),
           Espaco(),
-          BoxEspecialidade(especialidade: "Otorrino", press: () {}),
+          BoxEspecialidade(
+              especialidade: "Pediatra",
+              press: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => AllDoctors(
+                          especialidade: 'pediatra',
+                        )));
+              }),
           Espaco(),
-          BoxEspecialidade(especialidade: "Cardiologista", press: () {}),
+          BoxEspecialidade(
+              especialidade: "Clínico Geral",
+              press: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => AllDoctors(
+                          especialidade: 'clínico geral',
+                        )));
+              }),
           Espaco(),
-          BoxEspecialidade(especialidade: "Otorrino", press: () {}),
+          BoxEspecialidade(
+              especialidade: "Dermatologista",
+              press: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => AllDoctors(
+                          especialidade: 'dermatologista',
+                        )));
+              }),
           Espaco(),
-          BoxEspecialidade(especialidade: "Cardiologista", press: () {}),
-          Espaco(),
-          BoxEspecialidade(especialidade: "Otorrino", press: () {}),
-          Espaco(),
-          BoxEspecialidade(especialidade: "Cardiologista", press: () {}),
-          Espaco(),
-          BoxEspecialidade(especialidade: "Otorrino", press: () {}),
+          BoxEspecialidade(
+              especialidade: "Obstetra",
+              press: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => AllDoctors(
+                          especialidade: 'obstetra',
+                        )));
+              }),
+          // Espaco(),
+          // BoxEspecialidade(especialidade: "Otorrino", press: () {}),
+          // Espaco(),
+          // BoxEspecialidade(especialidade: "Cardiologista", press: () {}),
+          // Espaco(),
+          // BoxEspecialidade(especialidade: "Otorrino", press: () {}),
         ],
       ),
     );
